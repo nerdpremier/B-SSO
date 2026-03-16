eb5849836c4dc6b5ec28a87da66bbb9f0cec11c8
nerdpremier <touchpeera@gmail.com>
2026-03-17 01:21:05 +0700

    cars

--- api/auth.js
+++
@@ -1,4 +0,0 @@
-import '../startup-check.js';
-import authHandler from '../lib/auth/index.js';
-
-export default authHandler;
--- jest.config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.js'],
-};
-
